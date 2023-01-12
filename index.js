b57090d8831649ed0e572d1a9775146bacbcddda
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-12 16:34:30 -0500

    getting date query string in order

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //query select
 // <div id="current-date">todays date</div>
+const currentDate = document.querySelector('#current-date')
+const todaysDate = new Date().toISOString().slice(0,10)
+console.log( todaysDate)
+const dateURI = `https://www.hebcal.com/converter?cfg=json&date=${todaysDate}&g2h=1&strict=1`
+//api function to get hebrew date
+function getTodaysHebrewDate() {
+  fetch()
+}
 //on DOM load hit api and display today's civil and hebrew date
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('loaded')
+  // getTodaysHebrewDate()
+
+
+
+})
 // append data to div
-document.addEventListener('DOMContentLoaded', () => console.log('loaded'))
+
