4de032097d7ebd69a3663ea835c148ad2d7c37e2
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-12 16:38:00 -0500

    getting data back from API on DOM load

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 // <div id="current-date">todays date</div>
 const currentDate = document.querySelector('#current-date')
 const todaysDate = new Date().toISOString().slice(0,10)
-console.log( todaysDate)
+console.log(todaysDate)
 const dateURI = `https://www.hebcal.com/converter?cfg=json&date=${todaysDate}&g2h=1&strict=1`
 //api function to get hebrew date
 function getTodaysHebrewDate() {
-  fetch()
+  fetch(dateURI) 
+  .then(res => res.json())
+  .then(data => console.log(data))
 }
 //on DOM load hit api and display today's civil and hebrew date
 document.addEventListener('DOMContentLoaded', () => {
   console.log('loaded')
-  // getTodaysHebrewDate()
+  getTodaysHebrewDate()
 
 
 
