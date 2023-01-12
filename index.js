fac802f96e964ae95c4c14eff45832a2d303bdbf
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-12 16:16:50 -0500

    added testing console.log to verify index.js is hooked up

---
+++ index.js
@@ -0,0 +1,5 @@
+//query select
+// <div id="current-date">todays date</div>
+//on DOM load hit api and display today's civil and hebrew date
+// append data to div
+document.addEventListener('DOMContentLoaded', () => console.log('loaded'))
