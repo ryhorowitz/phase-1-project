627be67105c413c62e90f3c880cde2dca1630304
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-15 14:23:38 -0500

    form is being query selected and birthday value is being grabbed. now to continue with radio buttons

--- index.js
+++ index.js
@@ -3,29 +3,33 @@
 const currentDate = document.querySelector('#current-date')
 const cdate = document.querySelector('#current-civil-date')
 const hdate = document.querySelector('#current-hdate')
+const form = document.querySelector('#birthday-calc')
 
-const todaysDateQueryStr = new Date().toISOString().slice(0,10) 
-const todaysDateDisplayStr = new Date().toString().slice(0,15)
+const todaysDateQueryStr = new Date().toISOString().slice(0, 10)
+const todaysDateDisplayStr = new Date().toString().slice(0, 15)
 
 const dateURI = `https://www.hebcal.com/converter?cfg=json&date=${todaysDateQueryStr}&g2h=1&strict=1`
 //api function to get hebrew date
 function getTodaysHebrewDate() {
-  fetch(dateURI) 
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    const { hm, hd, hy, hebrew } = { ...data }
-    console.log('hm is', hebrew)
-    cdate.innerText = `${todaysDateDisplayStr}`
-    hdate.innerText = `${hm} ${hd}, ${hy} ${hebrew}`
-  })
+  fetch(dateURI)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data)
+      const { hm, hd, hy, hebrew } = { ...data }
+      cdate.innerText = `${todaysDateDisplayStr}`
+      hdate.innerText = `${hm} ${hd}, ${hy} ${hebrew}`
+    })
   // append data to div
 }
 //on DOM load hit api and display today's civil and hebrew date
 document.addEventListener('DOMContentLoaded', () => {
   getTodaysHebrewDate()
-
-
-
 })
 
+//on Form submit get birthday info
+// console.log('form is, ', form)
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const birthday = document.querySelector('input#birthday')
+  console.log(birthday.value)
+})
