ca8dabc28b1d5bd852f610dc32640bf491b8e223
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-15 17:54:06 -0500

    adding Hebrew Birthday to DOM

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //query select
 // <div id="current-date">todays date</div>
+const body = document.querySelector('body')
 const currentDate = document.querySelector('#current-date')
 const cdate = document.querySelector('#current-civil-date')
 const hdate = document.querySelector('#current-hdate')
@@ -32,23 +33,39 @@ function getTodaysHebrewDate() {
 document.addEventListener('DOMContentLoaded', () => {
   getTodaysHebrewDate()
 })
+function getHebrewBirthday(str) {
+  fetch(str)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    addBirthdayToDOM(data)
+    // with fetch data now display person's heebrew birthday on DOM
+    // add Birthday to DOM
+  })
+}
 
+function addBirthdayToDOM(data) {
+  const { hm, hd, hy, hebrew } = { ...data }
+  const birthdayDiv = document.createElement('div')
+  birthdayDiv.className = 'birthday-container'
+  const birthdayText = document.createElement('div')
+  birthdayText.id = 'birthday=text'
+  birthdayText.innerText = `Your Hebrew Birthday is ${hm} ${hd}, ${hy} ${hebrew}`
+  console.log(birthdayText)
+  birthdayDiv.appendChild(birthdayText)
+  body.append(birthdayDiv)
+}
 //on Form submit get birthday info
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const birthday = document.querySelector('input#birthday')
-  console.log('Birthday, ', birthday.value)
   const timesOfDay = document.querySelectorAll('input[name="time-of-day"]')
   const timeOfDay = radioChecked(timesOfDay)
- //with the selected time of day and date
- //query heb cal
-//  generate query String
-const qDate = formatQueryDate(birthday.value)
-const qString = `${URI}?cfg=json&date=${qDate}&g2h=1&strict=1&gs=on`
+  const gs = timeOfDay === 'evening' ? 'on' : 'off'
+  const qDate = formatQueryDate(birthday.value)
   console.log('qDate', qDate)
-  fetch(qString)
-    .then(res => res.json())
-    .then(data => console.log(data))
+  const qString = `${URI}?cfg=json&date=${qDate}&g2h=1&strict=1&gs=${gs}`//defaults to after sunset
+  getHebrewBirthday(qString)
 })
 
 function formatQueryDate(date) { //takes in 11/22/1988 returns 1988-11-22
