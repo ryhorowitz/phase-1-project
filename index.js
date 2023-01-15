fb10d257edbc1760424c8ac1348e551cf97aeb47
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-15 13:27:37 -0500

    displaying Hebrew Date on dom.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 //query select
 // <div id="current-date">todays date</div>
 const currentDate = document.querySelector('#current-date')
-const todaysDate = new Date().toISOString().slice(0,10)
-console.log(todaysDate)
-const dateURI = `https://www.hebcal.com/converter?cfg=json&date=${todaysDate}&g2h=1&strict=1`
+const hdate = document.querySelector('#current-hdate')
+const todaysDateStr = new Date().toISOString().slice(0,10)
+console.log(todaysDateStr)
+const dateURI = `https://www.hebcal.com/converter?cfg=json&date=${todaysDateStr}&g2h=1&strict=1`
 //api function to get hebrew date
 function getTodaysHebrewDate() {
   fetch(dateURI) 
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    const { hm, hd, hy } = { ...data }
+    console.log('hm is', hd)
+    hdate.innerText = `${hm} ${hd}, ${hy}`
+  })
+  // append data to div
 }
 //on DOM load hit api and display today's civil and hebrew date
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded')
   getTodaysHebrewDate()
 
 
 
 })
-// append data to div
 
